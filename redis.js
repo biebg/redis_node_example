6c7df187c6f0d60a930510d1f731991f68cd8863
yuansc <823374796@qq.com>
2014-02-28 18:09:23 +0800

    添加注释

--- redis.js
+++ redis.js
@@ -1,53 +1,58 @@
 /**
-* Created by a11 on 14-2-28.
-*/
-var redis   = require('redis');
-var client  = redis.createClient('6379', '127.0.0.1');
-client.on("error", function(error) {
+ * Created by a11 on 14-2-28.
+ */
+var redis = require('redis');
+//打开数据库，简历连接
+var client = redis.createClient('6379', '127.0.0.1');
+client.on("error", function (error) { //监控连接错误
     console.log(error);
 });
-client.select('0', function(error){
-    if(error) {
+//redis set操作
+client.select('0', function (error) {
+    if (error) {
         console.log(error);
     } else {
         // set
-        client.set('str_key_0', '0', function(error, res) {
-            if(error) {
+        client.set('str_key_0', '0', function (error, res) { //设置key为str_key_0的value为0
+            //插入成功返回OK结果
+            if (error) {
                 console.log(error);
             } else {
                 console.log(res);
-                client.get('str_key_0',function(error,result){
-                   if(error){
-                       console.log(error)
-                   }else{
-                       console.log(result)
-                   }
-                    // 关闭链接
-                    client.end();
-                })
-
+                client.get('str_key_0', function (error, result) {
+                        //在这里调用get的用法做校验，查看是否插入成功
+                        if (error) {
+                            console.log(error)
+                        } else {
+                            console.log(result)
+                        }
+                        // 关闭链接
+                        client.end();//关闭数据库连接，下同，不再解释
+                    }
+                )
             }
-
         });
     }
 });
 
-client.select('0', function(error){
-    if(error) {
+client.select('0', function (error) {
+    if (error) {
         console.log(error);
     } else {
         // hmset
         var info = {};
         info.baidu = 'www.baidu.com';
-        info.sina  = 'www.sina.com';
-        info.qq    = 'www.qq.com';
-        client.hmset('site', info, function(error, res){
-            if(error) {
+        info.sina = 'www.sina.com';
+        info.qq = 'www.qq.com';
+        client.hmset('site', info, function (error, res) {
+            //插入哈希数据，info为json格式
+            if (error) {
                 console.log(error);
             } else {
                 console.log(res);
-                client.hmget('site',['baidu','qq'],function(error,result){
-                    console.log(error,result)
+                client.hmget('site', ['baidu', 'qq'], function (error, result) {
+                    //获取哈希数据中的特定keys中的值，返回值为数组
+                    console.log(error, result)
                     client.end()
                 })
             }
@@ -57,29 +62,30 @@ client.select('0', function(error){
     }
 });
 
-client.select('0', function(error){
-    if(error) {
+client.select('0', function (error) {
+    if (error) {
         console.log(error);
     } else {
         // hgetall
-        client.hgetall('site', function(error, res){
-            if(error) {
+        client.hgetall('site', function (error, res) {
+            //获取site值对应的所有数据（表达不好，即获取整个hash值），返回值为json
+            if (error) {
                 console.log(error);
             } else {
                 console.log(res);
             }
-
             // 关闭链接
             client.end();
         });
     }
 });
 
-client.select('0', function(error){
-    if(error) {
+client.select('0', function (error) {
+    if (error) {
         console.log(error);
     } else {
         // lpush
+        //链表插入，插入多值，存储数据如同栈，最后进入的获取时在最上方
         client.lpush('list', 'key_0');
         client.lpush('list', 'key_1');
         client.lpush('list', 'key_2');
@@ -87,13 +93,14 @@ client.select('0', function(error){
         client.end();
     }
 });
-client.select('0', function(error){
-    if(error) {
+client.select('0', function (error) {
+    if (error) {
         console.log(error);
     } else {
         // lrange
-        client.lrange('list', '0', '-1', function(error, res){
-            if(error) {
+        //获取list链表中所有的数据，-1表示所有，也可为其他值，代表第0个到第X个
+        client.lrange('list', '0', '-1', function (error, res) {
+            if (error) {
                 console.log(error);
             } else {
                 console.log(res);
